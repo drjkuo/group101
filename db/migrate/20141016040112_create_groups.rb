e102344b9468968fb2af2c363ae85c668e05af31
drjkuo <drjkuo@gmail.com>
2014-10-16 23:29:38 +0800

    Initial Commit

---
+++ db/migrate/20141016040112_create_groups.rb
@@ -0,0 +1,15 @@
+class CreateGroups < ActiveRecord::Migration
+  def change
+    create_table :groups do |t|
+      t.string :title
+      t.terails :description
+      t.string :g
+      t.string :model
+      t.string :group
+      t.string :title
+      t.text :description
+
+      t.timestamps
+    end
+  end
+end
